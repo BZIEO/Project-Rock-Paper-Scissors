7c05ccbe6cf7540d453d0873e065d98f2d78f235
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-03-21 02:21:20 -0500

    RPS Progress

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const hands = ["rock", "paper", "scissor"];
+const choices = ["rock", "paper", "scissor"];
 
-function play(playerSelection, computerSelection) {
-  playerSelection = prompt("Rock paper or scissor?").toLowerCase();
-  computerSelection = function computerPlay() {
-    return hands[Math.floor(Math.random() * hands.length)];
-  };
+//Function to randomly return RPS//
+const computerSelection = function computerPlay() {
+  return choices[Math.floor(Math.random() * choices.length)];
+};
+
+//Function to play//
+function play(
+  playerSelection = prompt("rock paper or scissor").toLowerCase(),
+  computerSelection
+) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie! Try again to win";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lost! Paper beats rock.";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lost! Paper loses to scissors.";
+  } else if (playerSelection === "scissor" && computerSelection === "rock") {
+    return " You lost! Rock beats scissors";
+  } else if (playerSelection === "scissor" && computerSelection === "paper") {
+    return "You won! Scissor cuts paper;";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You won! Paper beats rock.";
+  } else if (playerSelection === "rock" && computerSelection === "scissor") {
+    return "You won! Rock beats scissors";
+  }
 }
